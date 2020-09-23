136803a9a14d424b0fde6fe26512dfe0079db9fd
Ben <bjmorse@gmail.com>
2020-09-22 22:27:11 -0700

    cant build, cmake gives generator expression error

---
+++ qt_hack.cmake
@@ -0,0 +1,14 @@
+# SET(_qt5_install_prefix /mnt/overflow/work/wasm-groot/lib/qt5/)
+# SET(Qt5_DIR /usr/lib/x86_64-linux-gnu/cmake/Qt5/)
+
+# include(/mnt/overflow/work/wasm-groot/lib/qt5/qtbase/lib/cmake/Qt5/Qt5Config.cmake)
+
+# SET(Qt5_DIR /mnt/overflow/work/wasm-groot/lib/qt5/qtbase/lib/cmake/Qt5)
+SET(Qt5_DIR ../../qt5/qtbase/lib/cmake/Qt5)
+
+
+# set(Qt5Core_DIR /mnt/overflow/work/wasm-groot/lib/qt5/qtbase/lib/cmake/Qt5Core)
+# set(Qt5_DIR /mnt/overflow/work/wasm-groot/lib/qt5/qtbase/lib/cmake/Qt5)
+# set(QT_QMAKE_EXECUTABLE /mnt/overflow/work/wasm-groot/lib/qt5/qtbase/bin/qmake)
+
+
